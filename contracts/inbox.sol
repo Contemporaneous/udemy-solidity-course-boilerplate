8d22cec84845e10051f7dfd9308dfe14ed19003b
Rhian McClelland <rhianmcclelland193@hotmail.com>
2022-04-19 15:33:37 +1000

    Correct case error

--- contracts/inbox.sol
+++ contracts/inbox.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.17;
 
-contract inbox {
+contract Inbox {
     string public message;
 
-    function Inbox(string initialMessage) public {
+    function Inbox(string initialMessage)public {
         message = initialMessage;
     }
 
